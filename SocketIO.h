b63445e4c8ed2340be9f083dcdd057ed041829d2
Noam Maimon <noam1222@gmail.com>
2023-01-15 18:16:43 +0200

    socketIO

---
+++ SocketIO.h
@@ -0,0 +1,30 @@
+#ifndef MULTICLASSIFIEDSERVER_SOCKETIO_H
+#define MULTICLASSIFIEDSERVER_SOCKETIO_H
+
+#include "DefaultIO.h"
+#include <string.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 1024
+#define MSG_END "EOF"
+
+using namespace std;
+
+/**
+ * class for sending and receiving data using sockets.
+ */
+class SocketIO : public DefaultIO{
+private:
+    int m_sockNum;
+    char m_buffer[BUFFER_SIZE];
+    size_t m_bufferSize;
+    void clearBuffer();
+    void sendBufferSizeOrLess(string s);
+public:
+    SocketIO(int sockNum);
+    string read() override;
+    void write(string s) override;
+};
+
+
+#endif //MULTICLASSIFIEDSERVER_SOCKETIO_H
